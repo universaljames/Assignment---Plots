be094edda8d874a427deb782fab22dff8741dcf9
ZHAO, Xiong <universaljames@me.com>
2020-05-25 08:08:13 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,6 +7,6 @@
   legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
          c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
  
- dev.copy(png,"plot2.png", width=480, height=480)
+ dev.copy(png,"plot3.png", width=480, height=480)
  
  dev.off()
